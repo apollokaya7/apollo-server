7a88afdeb4aa85513ed749336988f09d7d1ae863
apollokaya7 <rigerniclas@gmail.com>
2026-05-24 03:35:56 +0200

    Update server.py

--- server.py
+++ server.py
@@ -11,8 +11,7 @@ def home():
 def check():
     return jsonify({"valid": True})
 
-port = int(os.environ.get("PORT", 10000))
-
+# WICHTIG: Render Port ZWINGEND korrekt binden
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT"))
     app.run(host="0.0.0.0", port=port)
-
