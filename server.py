8eaa12b6f3399e059756d6d3d0eb87025b8491d1
apollokaya7 <rigerniclas@gmail.com>
2026-05-24 03:39:37 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,3 @@
-import os
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
@@ -10,8 +9,3 @@ def home():
 @app.route("/check", methods=["POST"])
 def check():
     return jsonify({"valid": True})
-
-# WICHTIG: Render Port ZWINGEND korrekt binden
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT"))
-    app.run(host="0.0.0.0", port=port)
